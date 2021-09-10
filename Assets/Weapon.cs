210c057cec84b78ab0d70c94af157cf8d4eedd12
Joseph <joseph.103@hotmail.es>
2021-09-10 13:34:07 -0500

    final

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,13 +6,19 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) 
+
+        if (Input.GetButtonDown("Fire1")) 
         {
+            animator.SetBool("IsShooting", true);
             Shoot();
+        }
+        else if(!Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("IsShooting", false);
         }
     }
 
